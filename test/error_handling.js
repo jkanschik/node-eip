5da0e729a040606070092deeac285af2bde3c28b
jkanschik <jkanschik@freenet.de>
2012-06-19 14:36:48 +0200

    Added default error handling route.

---
+++ test/error_handling.js
@@ -0,0 +1,28 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    Route = require("../lib/eip").Route;
+
+vows.describe('If errors occur').addBatch({
+	'with default error handling': {
+		topic: function() {
+			var r = new Route().process(function(event, cb) {
+				throw "Some exception";
+			});
+			r.errorRoute.process(this.callback);
+			r.inject();
+			r.shutDown();
+		},
+		'the error handling route should be invoked': function (event, callback) {
+			assert.isObject(event);
+			assert.equal(event.body, "Some exception");
+		}
+	}
+}).export(module);
+
+
+
+
+
+
+
+
