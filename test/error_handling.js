50797524b67b8d6573b7990162cc90f53b123210
jkanschik <jkanschik@freenet.de>
2012-06-19 14:41:13 +0200

    Added test for custom error route.

--- test/error_handling.js
+++ test/error_handling.js
@@ -16,6 +16,20 @@ vows.describe('If errors occur').addBatch({
 			assert.isObject(event);
 			assert.equal(event.body, "Some exception");
 		}
+	},
+	'with custom error route': {
+		topic: function() {
+			var r = new Route().process(function(event, cb) {
+				throw "Some exception";
+			});
+			r.errorRoute = new Route().log().process(this.callback);
+			r.inject();
+			r.shutDown();
+		},
+		'the error handling route should be invoked': function (event, callback) {
+			assert.isObject(event);
+			assert.equal(event.body, "Some exception");
+		}
 	}
 }).export(module);
 
