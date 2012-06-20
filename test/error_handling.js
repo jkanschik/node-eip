4ae41e4677de1e47bc0ddeb3b14c80988c9c7f6c
Jens Kanschik <jens_kanschik@freenet.de>
2012-06-20 16:06:56 +0200

    Implemented repeated calls of processor in case of an exception.

--- test/error_handling.js
+++ test/error_handling.js
@@ -2,19 +2,29 @@ var vows = require('vows'),
     assert = require('assert'),
     Route = require("../lib/eip").Route;
 
-vows.describe('If errors occur').addBatch({
+vows.describe('If processors throw an exception').addBatch({
 	'with default error handling': {
 		topic: function() {
+			var self = this;
+			this.numberOfAttempts = 0;
 			var r = new Route().process(function(event, cb) {
+				self.numberOfAttempts += 1;
 				throw "Some exception";
 			});
-			r.errorRoute.process(this.callback);
-			r.inject();
+			r.errorRoute
+				.process(function(event, cb){self.callback.call(self, event, cb)});
+			r.inject("Text");
 			r.shutDown();
 		},
 		'the error handling route should be invoked': function (event, callback) {
 			assert.isObject(event);
-			assert.equal(event.body, "Some exception");
+			assert.equal(event.headers._exception.cause, "Some exception");
+			assert.equal(event.headers._exception.numberOfAttempts, 3);
+			assert.isNotNull(event.headers._exception.timestamp);
+			assert.equal(event.body, "Text");
+		},
+		'the failing processor should be invoked three times': function (event, callback) {
+			assert.equal(this.numberOfAttempts, 3);
 		}
 	},
 	'with custom error route': {
@@ -23,17 +33,46 @@ vows.describe('If errors occur').addBatch({
 				throw "Some exception";
 			});
 			r.errorRoute = new Route().log().process(this.callback);
-			r.inject();
+			r.inject("Text");
 			r.shutDown();
 		},
 		'the error handling route should be invoked': function (event, callback) {
 			assert.isObject(event);
-			assert.equal(event.body, "Some exception");
+			assert.equal(event.headers._exception.cause, "Some exception");
+			assert.isNotNull(event.headers._exception.timestamp);
+			assert.equal(event.body, "Text");
 		}
 	}
 }).export(module);
 
-
+//vows.describe('If processors set the exception header').addBatch({
+//	'with default error handling': {
+//		topic: function() {
+//			var self = this;
+//			this.numberOfAttempts = 0;
+//			var r = new Route().process(function(event, cb) {
+//				self.numberOfAttempts += 1;
+//				event.headers._exception = {
+//					cause: "Some exception"
+//				};
+//			});
+//			r.errorRoute
+//				.process(function(event, cb){self.callback.call(self, event, cb)});
+//			r.inject("Text");
+//			r.shutDown();
+//		},
+//		'the error handling route should be invoked': function (event, callback) {
+//			assert.isObject(event);
+//			assert.equal(event.headers._exception.cause, "Some exception");
+//			assert.equal(event.headers._exception.numberOfAttempts, 3);
+//			assert.isNotNull(event.headers._exception.timestamp);
+//			assert.equal(event.body, "Text");
+//		},
+//		'the failing processor should be invoked three times': function (event, callback) {
+//			assert.equal(this.numberOfAttempts, 3);
+//		}
+//	}
+//}).export(module);
 
 
 
