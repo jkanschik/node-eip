98458943a7102931ecf344f146438035e7a7c5c7
Jens Kanschik <jens_kanschik@freenet.de>
2012-07-09 13:34:35 +0200

    Format objects in simple expression language as json.

--- test/expression_language.js
+++ test/expression_language.js
@@ -6,27 +6,7 @@ var vows = require('vows'),
 
 // Create a Test Suite
 vows.describe('For the simple expression language').addBatch({
-//	'when replacing ${body} with a string': {
-//		topic: function() {
-//			var r = new Route().simple("Replacing ${body}.")
-//				.process(this.callback);
-//			r.inject("value");
-//		},
-//		'the string should be inserted': function (event, callback) {
-//			assert.equal(event.body, "Replacing value.");
-//		}
-//	},
-//	'when using nested objects with dot notation': {
-//		topic: function() {
-//			var r = new Route().simple("String value: ${body.nested.stringValue} and integer value: ${body.nested.intValue}.")
-//				.process(this.callback);
-//			r.inject({nested: {stringValue: "value", intValue: 42}});
-//		},
-//		'string and integer values should be inserted': function (event, callback) {
-//			assert.equal(event.body, "String value: value and integer value: 42.");
-//		}
-//	},
-	'sel: when using simple attributes ': {
+	'when using simple attributes ': {
 		topic: function() {
 			var fn = sel.compile("String: ${string}; Integer: ${integer}.");
 			var r = fn({string:"value", integer:42});
@@ -36,17 +16,17 @@ vows.describe('For the simple expression language').addBatch({
 			assert.equal(result, "String: value; Integer: 42.");
 		}
 	},
-	'sel: when using nested objects with dot notation': {
+	'when using nested objects with dot notation': {
 		topic: function() {
 			var fn = sel.compile("String: ${strings.value}; Integer: ${integers.value}.");
 			var r = fn({strings:{value: "value"}, integers:{value:42}});
 			this.callback(r);
 		},
-		'string and integer valuess should be inserted': function (result, callback) {
+		'string and integer values should be inserted': function (result, callback) {
 			assert.equal(result, "String: value; Integer: 42.");
 		}
 	},
-	'sel: when using nested objects are not defined': {
+	'when using nested objects are not defined': {
 		topic: function() {
 			var fn = sel.compile("string.missing = ${strings.missing}; missing.value = ${missing.value}; missing = ${missing}");
 			var r = fn({strings:{value: "value"}});
@@ -56,7 +36,7 @@ vows.describe('For the simple expression language').addBatch({
 			assert.equal(result, "string.missing = ; missing.value = ; missing = ");
 		}
 	},
-	'sel: when attributes contain characters like "_"': {
+	'when attributes contain characters like "_"': {
 		topic: function() {
 			var fn = sel.compile("${model._id}");
 			var r = fn({model:{_id: "value"}});
@@ -65,7 +45,20 @@ vows.describe('For the simple expression language').addBatch({
 		'the value should be replaced': function (result, callback) {
 			assert.equal(result, "value");
 		}
+	},
+	'when replacing objects': {
+		topic: function() {
+			var fn = sel.compile("${some.object}.");
+			var object = {attr: "attr", int: 42, nested: {attr2:"attr2"}}
+			var r = fn({some:{object: object}});
+			this.callback(r);
+		},
+		'string and integer values should be inserted': function (result, callback) {
+			assert.equal(result, "{ attr: 'attr', int: 42, nested: { attr2: 'attr2' } }.");
+		}
 	}
+
+	
 
 	
 }).export(module);
